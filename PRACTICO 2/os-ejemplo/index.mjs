b827a484a99889c76a08684b9b40ace5f5de6923
pablosegundo31 <pablosegundolobos18@gmail.com>
2025-02-11 07:50:42 -0300

    nuevo 1.0

---
+++ PRACTICO 2/os-ejemplo/index.mjs
@@ -0,0 +1,16 @@
+import OS from 'os';
+
+console.log('arquitectura:', OS.arch());
+
+console.log('Plataforma:', OS.platform());
+
+console.log('Memoria Total:', OS.totalmem());
+
+console.log('Memoria Libre:' , OS.freemem());
+
+console.log('informacion de CPU:' , OS.cpus());
+
+
+
+
+
