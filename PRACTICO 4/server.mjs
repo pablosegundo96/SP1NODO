c25f12bbfc3be58482af8d0e29545a0a80997c40
pablosegundo31 <pablosegundolobos18@gmail.com>
2025-02-26 20:58:30 -0300

    nuevo

---
+++ PRACTICO 4/server.mjs
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express ();
+const PORT = 3000;
+
+app.get('/profile', (req, res) => {
+    const edad = req.query.edad;
+    console.log(`Edad recibida: ${edad}`);
+
+    res.send(`Edad del perfil: ${edad}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en htto:localhost:${PORT}`);
+    
+});
