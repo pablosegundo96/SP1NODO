777d6b7b997a57500c1b0156850694cd4f983486
pablosegundo31 <pablosegundolobos18@gmail.com>
2025-03-05 18:09:49 -0300

    nuevo

---
+++ PRACTICO 4/project-root/repository/superheroesDataSource.mjs
@@ -0,0 +1,5 @@
+export default class superheroresDataSource {
+    obtenerTodos() {
+        throw new Error('Este metodo debe ser implementado por la sub clase');
+    }
+}
---
+++ PRACTICO 4/project-root/views/responseView.mjs
@@ -0,0 +1,8 @@
+export function renderizarSuperheroe(superheroe){
+    return JSON.stringify(superheroe, null, 2);
+
+}
+
+export function renderizarListaSuperheroes(superheroe) {
+    return JSON.stringify(superheroe, null, 2);
+}
